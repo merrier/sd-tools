e03c32e9ed66d2123953b0f31bf91675abd34508
wangcong.merrier <wangcong.merrier@bytedance.com>
2018-11-23 20:21:05 +0800

    Fix:http->https

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 $(function(){
 
-    window.odometerOptions = {
-        auto: false, // Don't automatically initialize everything with class 'odometer'
-        selector: '.number-item', // Change the selector used to automatically find things to be animated
-        format: 'dd', // Change how digit groups are formatted, and how many digits are shown after the decimal point
-        duration: 3000, // Change how long the javascript expects the CSS animation to take
-        animation: 'count' // Count is a simpler animation method which just increments the value,
-                           // use it when you're looking for something more subtle.
-    };
+    // window.odometerOptions = {
+    //     auto: false, // Don't automatically initialize everything with class 'odometer'
+    //     selector: '.number-item', // Change the selector used to automatically find things to be animated
+    //     format: 'dd', // Change how digit groups are formatted, and how many digits are shown after the decimal point
+    //     duration: 3000, // Change how long the javascript expects the CSS animation to take
+    //     animation: 'count' // Count is a simpler animation method which just increments the value,
+    //                        // use it when you're looking for something more subtle.
+    // };
 
     Array.prototype.shuffle = function() {
         var input = this;
